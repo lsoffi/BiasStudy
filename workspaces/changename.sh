ba6de11ab9994d1dd214fd3041575d66ddc9dedc
livia <livia.soffi@cern.ch>
2015-02-25 18:38:19 +0100

    first

---
+++ workspaces/changename.sh
@@ -0,0 +1,34 @@
+mv  HighMass-hgg.m150.0_w0_inputBias_truth_8TeV_132_313.root          HighMass-hgg.m150.0_w0_inputBias_truth_8TeV_FIX.root   
+mv  HighMass-hgg.m150.0_w10_inputBias_truth_8TeV_132_313.root   	  HighMass-hgg.m150.0_w10_inputBias_truth_8TeV_FIX.root  
+mv  HighMass-hgg.m250.0_w0_inputBias_truth_8TeV_187_424.root    	  HighMass-hgg.m250.0_w0_inputBias_truth_8TeV_FIX.root   
+mv  HighMass-hgg.m250.0_w10_inputBias_truth_8TeV_187_424.root   	  HighMass-hgg.m250.0_w10_inputBias_truth_8TeV_FIX.root  
+mv  HighMass-hgg.m350.0_w0_inputBias_truth_8TeV_243_536.root    	  HighMass-hgg.m350.0_w0_inputBias_truth_8TeV_FIX.root   
+mv  HighMass-hgg.m350.0_w10_inputBias_truth_8TeV_243_536.root   	  HighMass-hgg.m350.0_w10_inputBias_truth_8TeV_FIX.root  
+mv  HighMass-hgg.m450.0_w0_inputBias_truth_8TeV_298_647.root    	  HighMass-hgg.m450.0_w0_inputBias_truth_8TeV_FIX.root   
+mv  HighMass-hgg.m450.0_w10_inputBias_truth_8TeV_298_647.root   	  HighMass-hgg.m450.0_w10_inputBias_truth_8TeV_FIX.root  
+mv  HighMass-hgg.m550.0_w0_inputBias_truth_8TeV_354_759.root    	  HighMass-hgg.m550.0_w0_inputBias_truth_8TeV_FIX.root   
+mv  HighMass-hgg.m550.0_w10_inputBias_truth_8TeV_354_759.root   	  HighMass-hgg.m550.0_w10_inputBias_truth_8TeV_FIX.root  
+mv  HighMass-hgg.m650.0_w0_inputBias_truth_8TeV_410_871.root    	  HighMass-hgg.m650.0_w0_inputBias_truth_8TeV_FIX.root   
+mv  HighMass-hgg.m650.0_w10_inputBias_truth_8TeV_410_871.root   	  HighMass-hgg.m650.0_w10_inputBias_truth_8TeV_FIX.root  
+mv  HighMass-hgg.m750.0_w0_inputBias_truth_8TeV_465_982.root    	  HighMass-hgg.m750.0_w0_inputBias_truth_8TeV_FIX.root  
+mv  HighMass-hgg.m750.0_w10_inputBias_truth_8TeV_465_982.root   	  HighMass-hgg.m750.0_w10_inputBias_truth_8TeV_FIX.root 
+mv  HighMass-hgg.m850.0_w0_inputBias_truth_8TeV_521_1094.root   	  HighMass-hgg.m850.0_w0_inputBias_truth_8TeV_FIX.root  
+mv  HighMass-hgg.m850.0_w10_inputBias_truth_8TeV_521_1094.root  	  HighMass-hgg.m850.0_w10_inputBias_truth_8TeV_FIX.root 
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
